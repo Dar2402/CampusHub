6800bc7d0a9a4a4599833a797ffa152afb578695
Dar2402 <darshanrpandey243@gmail.com>
2025-03-07 13:08:06 +0530

    Added db config

--- college_management_system/production_settings.py
+++ college_management_system/production_settings.py
@@ -89,10 +89,12 @@ WSGI_APPLICATION = 'college_management_system.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'django',
@@ -101,7 +103,7 @@ DATABASES = {
     #     'HOST': '127.0.0.1',
     #     'PORT': '3307'
     # }
-}
+
 
 
 # Password validation
